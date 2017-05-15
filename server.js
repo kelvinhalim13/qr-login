f121a20959a8a841ffaf988dd97bb7c2c8bc54ee
vinzi13 <kelvinhalim45@gmail.com>
2017-05-15 16:06:10 +0700

    as

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var server= http.createServer(function(request, response) {
         response.writeHead(200, {"Content-Type": "text/plain","Access-Control-Allow-Origin":"*"});
 
 
-
+    console.log(ipaddress);
         process.on('uncaughtException', function(err)
         {
 	        response.end("Exception");
