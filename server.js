af4c06830cfd72a809652b017980bcb2d5efe6a6
vinzi13 <kelvinhalim45@gmail.com>
2017-05-15 15:49:33 +0700

    test

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
+// server.listen(server_port, server_ip_address, function () {
+//     console.log( "Listening on " + server_ip_address + ", port " + server_port )
+// });
 var http = require("http");
 
 var server= http.createServer(function(request, response) {
@@ -9,6 +11,7 @@ var server= http.createServer(function(request, response) {
         response.writeHead(200, {"Content-Type": "text/plain","Access-Control-Allow-Origin":"*"});
 
 
+        console.log(ipaddress);
         process.on('uncaughtException', function(err)
         {
 	        response.end("Exception");
