2ead92a2ed331c5c94fd1f4557d1d46a34fad186
vinzi13 <kelvinhalim45@gmail.com>
2017-05-15 16:04:42 +0700

    test

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 // });
 var http = require("http");
 
+console.log(ipaddress);
 var server= http.createServer(function(request, response) {
 
         response.writeHead(200, {"Content-Type": "text/plain","Access-Control-Allow-Origin":"*"});
 
 
-        console.log(ipaddress);
+
         process.on('uncaughtException', function(err)
         {
 	        response.end("Exception");
